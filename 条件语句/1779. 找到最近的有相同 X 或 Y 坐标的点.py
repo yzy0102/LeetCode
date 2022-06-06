dfb72686f303f4b987cb5d5e2e98f15570cf27a4
yzy0102 <1273160817@qq.com>
2022-06-06 18:45:12 +0800

    1779一遍过

--- 条件语句/1779. 找到最近的有相同 X 或 Y 坐标的点.py
+++ 条件语句/1779. 找到最近的有相同 X 或 Y 坐标的点.py
@@ -7,13 +7,21 @@ class Solution(object):
         :rtype: int
         """
 
-
+        max_distance = 10**5
+        min_index = -1
+        for index, point in enumerate(points):
+            if point[0] == x or point[1] == y:
+                manhattan_distance = abs(x - point[0]) + abs(y - point[1])
+                if manhattan_distance < max_distance:
+                    max_distance = manhattan_distance
+                    min_index = index
+        return min_index
 
 
 if __name__ == '__main__':
     S = Solution()
     x = 3
     y = 4
-    points = [[1,2],[3,1],[2,4],[2,3],[4,4]]
+    points = [[1,2]]
     s = S.nearestValidPoint(x, y, points)
     print(s)
