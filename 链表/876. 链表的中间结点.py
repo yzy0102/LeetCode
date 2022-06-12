a01a03cada790c355e3a874dfc85631f7472e640
yzy0102 <54012483+yzy1273160817@users.noreply.github.com>
2022-06-12 22:59:45 +0800

    876 快慢指针很关键

---
+++ 链表/876. 链表的中间结点.py
@@ -0,0 +1,51 @@
+#定义节点
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+#将传入的数组转化为链表
+def create_linked_list(arr):
+    head = ListNode(arr[0])
+    cur = head
+    for i in range(1, len(arr)):
+        cur.next = ListNode(arr[i])
+        cur = cur.next
+    return head
+
+#传入链表头节点，以数组形式返回
+def print_linked_list(head):
+    cur = head
+    res = []
+    while cur:
+        res.append(cur.val)
+        cur = cur.next
+    return res
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # n = 0
+        # result = head
+        # while head:
+        #     head = head.next
+        #     n += 1
+        # for i in range(n // 2):
+        #     result = result.next
+        # return result
+    # 可以使用快慢指针法, 快的走2步, 慢的走1步
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+if __name__ == "__main__":
+    head = create_linked_list([1, 2, 3, 4, 5])
+    solution = Solution()
+    sorted_lists = solution.middleNode(head)
+    print(sorted_lists)
